fb2ad71a14250e862c28bec81ee83a0213288ed7
sravya-lakkoju <sravyalakkoju1161@gmail.com>
2025-06-25 11:56:55 +0530

    Create 5.rs

---
+++ 5.rs
@@ -0,0 +1,13 @@
+/*
+Write a program to implement the following
+a. Implicit type declaration
+b. Explicit type declaration
+*/
+
+fn main() {
+    let  a = 5;
+    let b: i32 = 10;
+    println!("a = {}",a);
+    println!("b = {}",b);
+   
+}
