36b0dabc67e28ce3d3eb04f4a6e1c66f01fde72d
sravya-lakkoju <sravyalakkoju1161@gmail.com>
2025-06-25 11:57:45 +0530

    Create 6.rs

---
+++ 6.rs
@@ -0,0 +1,10 @@
+/*
+Write Program to Declare an array, arr, of size 6 that has numbers divisible by 2 ranging
+from 0 to 10 and Print the value of arr.
+*/
+
+fn main() {
+    let arr =[0,1,2,3,4,5];
+    println!("Array: {:?}", arr);
+   
+}
