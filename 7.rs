19eeb71c1b57f17d3389a0e8ef74e82b72d224b0
sravya-lakkoju <sravyalakkoju1161@gmail.com>
2025-06-25 11:59:01 +0530

    Create 7.rs

---
+++ 7.rs
@@ -0,0 +1,10 @@
+/*
+Write a program to create and access a tuple.
+*/
+
+fn main() {
+    let my_tuple =(10,20, "Rust");
+    println!("First Element: {}", my_tuple.0);
+    println!("Second Element: {}", my_tuple.1);
+    println!("Third Element: {}", my_tuple.2);
+}
